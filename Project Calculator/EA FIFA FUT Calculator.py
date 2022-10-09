b116c974053b60493f23e9a5e45b0dfa595526ce
GuilhermeAfuso <guilherme.afuso@gmail.com>
2022-10-09 18:38:28 -0300

    Alteração das linhas
    Incremento de linhas para padronização.

--- Project Calculator/EA FIFA FUT Calculator.py
+++ Project Calculator/EA FIFA FUT Calculator.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 # EA FIFA FUT Calculator
 
 buy_price = int(input('Enter the purchase price: '))
@@ -22,23 +16,18 @@ ea_tax = sell_price * 0.05
 
 result = sell_price - ea_tax - buy_price
 
-def abc():
-    print(f'Buy price: {buy_price:_.0f}'.replace('.',',').replace('_','.'))
-    print(f'Sell price: {sell_price:_.0f}'.replace('.',',').replace('_','.'))
-    print(f'EA Tax: {ea_tax:_.0f}'.replace('.',',').replace('_','.'))
+
+def check():
+    print(f'Buy price: {buy_price:,.0f}')
+    print(f'Sell price: {sell_price:,.0f}')
+    print(f'EA Tax: {ea_tax:,.0f}')
 
     if result > 0:
-        print(f'Profit: {result:_.0f}'.replace('.',',').replace('_','.'))
+        print(f'Profit: {result:,.0f}')
     elif result == 0:
         print("You don't have profit.")
     else:
-        print(f'Loss: {result:_.0f}'.replace('.',',').replace('_','.'))
-
-abc()
+        print(f'Loss: {result:,.0f}')
 
 
-# In[ ]:
-
-
-
-
+check()
